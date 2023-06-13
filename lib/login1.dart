6b65121d1555d1eb874e090c110b991d7f79bfc2
tanawatgithub <tanawat.cpt@mail.kmutt.ac.th>
2023-06-13 15:28:09 +0700

    finished project

--- lib/login1.dart
+++ lib/login1.dart
@@ -255,7 +255,7 @@ class _LoginPageState extends State<LoginPage> {
                           },
                           child: const Text(
                             'เข้าสู่ระบบด้วยระบบ PIN',
-                            style: TextStyle(fontFamily:'Kanit',fontSize: 16,color: Colors.red,decoration: TextDecoration.underline,),
+                            style: TextStyle(fontFamily:'Kani',fontSize: 16,color: Colors.red,decoration: TextDecoration.underline,),
                           ),
                         ),
                       ],
