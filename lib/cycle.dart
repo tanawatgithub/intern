e219d2456be03bd0f361d82e2be7419bd5252a13
tanawatgithub <tanawat.cpt@mail.kmutt.ac.th>
2023-06-14 18:04:28 +0700

    finished project

--- lib/cycle.dart
+++ lib/cycle.dart
@@ -14,7 +14,19 @@ class _cyclepageState extends State<cyclepage> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.white,
-        title: Text('My Page'),
+        title: Row(
+          children: [
+            Spacer(),
+            Transform.translate(
+              offset: Offset(-1150, 0), // เลือนตำแหน่งรูปภาพทางขวาประมาณ -1150 หน่วย
+              child: Image.asset(
+                'assets/images/Zeenlogo1.png',
+                width: 100,
+                height: 100,
+              ),
+            ),
+          ],
+        ),
         actions: [
           ButtonBar(
             children: _buttonListItems.map<Widget>((String value) {
@@ -26,14 +38,70 @@ class _cyclepageState extends State<cyclepage> {
                   });
                 },
               );
-            })
-                .toList(),
+            }).toList(),
+          ),
+        ],
+        bottom: PreferredSize(
+          preferredSize: Size.fromHeight(2.0),
+          child: Container(
+            color: Colors.red, // สีแดง
+            height: 2.0, // ความสูงของเส้นขอบ
+          ),
+        ),
+      ),
+      body: Column(
+        children: [
+          Container(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                children: [
+                  Spacer(),
+                  Transform.translate(
+                    offset: Offset(-1450, 0),
+                    child: Text(
+                      'Cycle',
+                      style: TextStyle(
+                        fontFamily: 'Kanit',
+                        fontSize: 20,
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Container(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                children: [
+                  SizedBox(width: 25,),
+                  Container(
+                    width: 100,
+                    height: 100,
+                    child: Center(child: Text('1'),),
+                    decoration: BoxDecoration(
+                      color: Color.fromRGBO(255, 190, 82, 1),
+                      borderRadius: BorderRadius.only(topLeft: Radius.circular(20)),
+                    ),
+                  ),
+                  Container(
+                    width: 1241,
+                    height: 100,
+                    decoration: BoxDecoration(
+                      color: Color.fromRGBO(255, 255, 255, 1),
+                      borderRadius: BorderRadius.only(topRight: Radius.circular(20)),
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ),
         ],
       ),
-      body: Center(
-        child: Text('Content goes heree'),
-      ),
+
     );
   }
 }
