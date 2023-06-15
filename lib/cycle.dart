9e7615fc81bfde1f88f36c98a88c30227823e506
tanawatgithub <tanawat.cpt@mail.kmutt.ac.th>
2023-06-15 11:17:51 +0700

    finished project

--- lib/cycle.dart
+++ lib/cycle.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:intern/login1.dart';
+
 class cyclepage extends StatefulWidget {
   final String? txtname;
   final String? txtpassword;
@@ -9,12 +10,9 @@ class cyclepage extends StatefulWidget {
 
   @override
   _cyclepageState createState() => _cyclepageState();
-
 }
 
 class _cyclepageState extends State<cyclepage> {
-
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,7 +26,7 @@ class _cyclepageState extends State<cyclepage> {
             Text('Org คือ: ${widget.txtorg}'),
             Spacer(),
             Transform.translate(
-              offset: Offset(-1350, 0), // เลือนตำแหน่งรูปภาพทางขวาประมาณ -1150 หน่วย
+              offset: Offset(-1350, 0), // เลื่อนตำแหน่งรูปภาพทางขวาประมาณ -1350 หน่วย
               child: Image.asset(
                 'assets/images/Zeenlogo1.png',
                 width: 100,
@@ -37,7 +35,6 @@ class _cyclepageState extends State<cyclepage> {
             ),
           ],
         ),
-
         bottom: PreferredSize(
           preferredSize: Size.fromHeight(2.0),
           child: Container(
@@ -55,7 +52,7 @@ class _cyclepageState extends State<cyclepage> {
                 children: [
                   Spacer(),
                   Transform.translate(
-                    offset: Offset(-1410, 0),
+                    offset: Offset(-610, 0),
                     child: Text(
                       'Cycle',
                       style: TextStyle(
@@ -63,6 +60,36 @@ class _cyclepageState extends State<cyclepage> {
                         fontSize: 20,
                         color: Colors.grey,
                       ),
+                    ),
+                  ),
+                  Spacer(),
+                  ElevatedButton(
+                    onPressed: () => BackButton,
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.orange,
+                      onPrimary: Colors.white,
+                      onSurface: Colors.orange,
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: const [
+                        SizedBox(
+                          width: 10,
+                          height: 20,
+                        ),
+                        Icon(
+                          Icons.arrow_back,
+                          color: Colors.white,
+                        ),
+                        Text(
+                          'ย้อนกลับ',
+                          style: TextStyle(
+                            fontSize: 15,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+
+                      ],
                     ),
                   ),
                 ],
@@ -74,14 +101,20 @@ class _cyclepageState extends State<cyclepage> {
               padding: const EdgeInsets.all(8.0),
               child: Row(
                 children: [
-                  SizedBox(width: 30,),
+                  SizedBox(
+                    width: 30,
+                  ),
                   Container(
                     width: 100,
                     height: 100,
-                    child: Center(child: Text('1'),),
+                    child: Center(
+                      child: Text('1'),
+                    ),
                     decoration: BoxDecoration(
                       color: Color.fromRGBO(255, 190, 82, 1),
-                      borderRadius: BorderRadius.only(topLeft: Radius.circular(20)),
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(20),
+                      ),
                     ),
                   ),
                   Container(
@@ -89,7 +122,9 @@ class _cyclepageState extends State<cyclepage> {
                     height: 100,
                     decoration: BoxDecoration(
                       color: Color.fromRGBO(255, 255, 255, 1),
-                      borderRadius: BorderRadius.only(topRight: Radius.circular(20)),
+                      borderRadius: BorderRadius.only(
+                        topRight: Radius.circular(20),
+                      ),
                     ),
                   ),
                 ],
@@ -98,7 +133,6 @@ class _cyclepageState extends State<cyclepage> {
           ),
         ],
       ),
-
     );
   }
 }
